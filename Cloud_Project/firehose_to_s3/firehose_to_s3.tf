90b751a3e55e8bdad6320e0a3ebe02ed18c57949
Paulo Henrique Lacerda <65253405+phlacerda@users.noreply.github.com>
2021-02-01 15:42:38 -0300

    Delete some codes

--- Cloud_Project/firehose_to_s3/firehose_to_s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
-  name        = "terraform-kinesis-firehose-test-stream"
-  destination = "s3"
-
-  s3_configuration {
-    role_arn   = aws_iam_role.firehose_role.arn
-    bucket_arn = aws_s3_bucket.bucket.arn
-  }
-}
--- Cloud_Project/s3/s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "s3-project-bucket"
-  acl    = "private"
-}
